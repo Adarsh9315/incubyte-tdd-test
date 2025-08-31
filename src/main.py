0c1af0f0fa0870d4fb40da32b78e5f81286eb5c9
Adarsh Kumar <adarshkumar9315@gmail.com>
2025-08-31 16:43:47 +0530

    Step 2: Single number → itself

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 class StringCalculator:
     def add(self, numbers: str) -> int:
-        """
-        Step 1: Empty string should return 0.
-        """
+        # Step 1: Empty string should return 0.
         if numbers == "":
             return 0
+        
+        # Step 2: return the number itself if only one
+        return int(numbers)
--- src/test.py
+++ src/test.py
@@ -1,5 +1,12 @@
 from main import StringCalculator
 
+# Step 1: empty string returns 0
 def test_empty_string_returns_0():
     sc = StringCalculator()
     assert sc.add("") == 0
+
+
+# Step 2: single number returns its value
+def test_single_number_returns_value():
+    sc = StringCalculator()
+    assert sc.add("1") == 1
